5d48462a565fb03bad9dd268f877c150ec7ab356
EmiPeralta <emilianop96@hotmail.com>
2020-10-21 17:46:45 -0300

    Making the navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Youtube from './api/Youtube';
 import Navbar from './components/Navbar';
 
 const App = () => {
+	const [searchText, setSearchText] = useState('');
+
 	return (
 		<div className="App">
 			<Navbar />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,38 @@
 import React from 'react';
 import './Navbar.css';
 import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search'
+import SearchIcon from '@material-ui/icons/Search';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import { Avatar } from '@material-ui/core';
 
 const Navbar = () => {
+    const submitHandler = e => {
+        console.log(e);
+    };
+
     return (
         <div className="navbar">
             <div className="navbar__left">
-                <MenuIcon />
+                <MenuIcon className="navbar__menuLeft" />
                 <img
-                    className="navbar__logo"
+                    className="navbar__logoLeft"
                     src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
                     alt="youtube logo"
                 />
             </div>
-            <div className="navbar__input">
-                <input type="text" />
-                <SearchIcon />
-            </div>
+            <form>
+                <div className="navbar__input">
+                    <input type="text" placeholder="Search" />
+                    <SearchIcon className="navbar__inputButton" />
+                </div>
+            </form>
             <div className="navbar__icons">
-                <VideoCallIcon />
-                <AppsIcon />
-                <NotificationsIcon />
-                <Avatar />
+                <VideoCallIcon className="navbar__icon" />
+                <AppsIcon className="navbar__icon" />
+                <NotificationsIcon className="navbar__icon" />
+                <Avatar className="navbar__avatar" />
             </div>
         </div>
     );
